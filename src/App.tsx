7b62a80dd5b59a103016adffa00bf2031c71c021
Bank <wangyinhang@qq.com>
2021-09-27 22:25:07 +0800

    login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React, { FC } from 'react';
-import { Button } from 'antd';
 import './App.css';
+import Login from './pages/login';
 
-const App: FC = () => (
-  <div className="App">
-    <Button type="primary">Button</Button>
-  </div>
-);
+function App() {
+  return (
+    <Login></Login>
+  );
+}
 
 export default App;
